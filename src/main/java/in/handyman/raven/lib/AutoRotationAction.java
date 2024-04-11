51eee75d4f88c81071b6666ce080a84cdc59ec49
manikandan-tm <manikandan.tm@zucisystems.com>
2024-04-11 10:40:11 +0530

    1. changes in merger for index out of bound exception in logger 2. code refactor

--- src/main/java/in/handyman/raven/lib/AutoRotationAction.java
+++ src/main/java/in/handyman/raven/lib/AutoRotationAction.java
@@ -1,6 +1,5 @@
 package in.handyman.raven.lib;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import in.handyman.raven.exception.HandymanException;
 import in.handyman.raven.lambda.access.ResourceAccess;
 import in.handyman.raven.lambda.action.ActionExecution;
@@ -10,7 +9,6 @@ import in.handyman.raven.lib.model.AutoRotation;
 import in.handyman.raven.lib.model.autorotation.AutoRotationConsumerProcess;
 import in.handyman.raven.lib.model.autorotation.AutoRotationInputTable;
 import in.handyman.raven.lib.model.autorotation.AutoRotationOutputTable;
-import okhttp3.MediaType;
 import org.jdbi.v3.core.Jdbi;
 import org.jdbi.v3.core.argument.Arguments;
 import org.jdbi.v3.core.argument.NullArgument;
@@ -35,8 +33,6 @@ import java.util.stream.Collectors;
         actionName = "AutoRotation"
 )
 public class AutoRotationAction implements IActionExecution {
-  private static final MediaType MediaTypeJSON = MediaType
-          .parse("application/json; charset=utf-8");
   public static final String DEFAULT_SOCKET_TIME_OUT = "100";
   public static final String COPRO_CLIENT_SOCKET_TIMEOUT = "copro.client.socket.timeout";
   public static final String COPRO_CLIENT_API_SLEEPTIME = "copro.client.api.sleeptime";
@@ -49,7 +45,6 @@ public class AutoRotationAction implements IActionExecution {
   public static final String COLUMN_LIST = "origin_id,group_id,tenant_id,template_id,process_id, processed_file_path,paper_no, status,stage,message,created_on,root_pipeline_id,model_name,model_version";
   public static final String VAL_STRING_LIST = "VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
   public static final String READ_BATCH_SIZE = "read.batch.size";
-  public static final String COPRO_AUTOROTATION_URL1 = "copro.autorotation.url";
   private final int threadSleepTime;
   private final Integer consumerApiCount;
   private final Integer writeBatchSize;
@@ -60,13 +55,12 @@ public class AutoRotationAction implements IActionExecution {
   private final String outputDir;
   private final String insertQuery;
 
-  private int timeout;
+  private final int timeout;
 
   private final ActionExecutionAudit action;
   private final Logger log;
   private final AutoRotation autoRotation;
   private final Marker aMarker;
-  private final ObjectMapper mapper = new ObjectMapper();
   private final String URI;
   private final int readBatchSize;
 
@@ -79,17 +73,17 @@ public class AutoRotationAction implements IActionExecution {
     this.aMarker = MarkerFactory.getMarker(" AutoRotation:" + this.autoRotation.getName());
     this.URI = ((AutoRotation) autoRotation).getEndPoint();
     String socketTimeStr = action.getContext().get(COPRO_CLIENT_SOCKET_TIMEOUT);
-    socketTimeStr = socketTimeStr != null && socketTimeStr.trim().length() > 0 ? socketTimeStr : DEFAULT_SOCKET_TIME_OUT;
+    socketTimeStr = socketTimeStr != null && !socketTimeStr.trim().isEmpty() ? socketTimeStr : DEFAULT_SOCKET_TIME_OUT;
     this.timeout = Integer.parseInt(socketTimeStr);
     String threadSleepTimeStr = action.getContext().get(COPRO_CLIENT_API_SLEEPTIME);
-    threadSleepTimeStr = threadSleepTimeStr != null && threadSleepTimeStr.trim().length() > 0 ? threadSleepTimeStr : THREAD_SLEEP_TIME;
+    threadSleepTimeStr = threadSleepTimeStr != null && !threadSleepTimeStr.trim().isEmpty() ? threadSleepTimeStr : THREAD_SLEEP_TIME;
     this.threadSleepTime = Integer.parseInt(threadSleepTimeStr);
     //TODO - Please do same as above to ensure that you don't assume default values
     String consumerApiCountStr = this.action.getContext().get(CONSUMER_API_COUNT);
     consumerApiCount = Integer.valueOf(consumerApiCountStr);
     String writeBatchSizeStr = this.action.getContext().get(WRITE_BATCH_SIZE);
     this.writeBatchSize = Integer.valueOf(writeBatchSizeStr);
-    this.readBatchSize = Integer.valueOf(action.getContext().get(READ_BATCH_SIZE));
+    this.readBatchSize = Integer.parseInt(action.getContext().get(READ_BATCH_SIZE));
     this.schemaName = DEFAULT_INFO_SCHEMA_NAME;
     this.targetTableName = AUTO_ROTATION;
     this.columnList = COLUMN_LIST;
