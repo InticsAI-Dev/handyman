04847688609f0de06aba12fadcd7c9e2401981f8
iswerya-justin <iswerya.justin@zucisystems.com>
2024-05-22 16:59:14 +0530

    figure detection macro

---
+++ src/main/java/in/handyman/raven/lib/model/figure/detection/FigureDetectionQueryInputTable.java
@@ -0,0 +1,30 @@
+package in.handyman.raven.lib.model.figure.detection;
+
+import in.handyman.raven.lib.CoproProcessor;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class FigureDetectionQueryInputTable implements CoproProcessor.Entity{
+    private String originId;
+    private String inputFilePath;
+    private Integer paperNo;
+    private Long rootPipelineId;
+    private Long tenantId;
+    private Long groupId;
+    private Long processId;
+    private String process;
+    private Float threshold;
+
+    @Override
+    public List<Object> getRowData() {
+        return null;
+    }
+}
