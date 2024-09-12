72c92cc51028360f5c4a39fefbe2d3c55d5c3f26
anandhh2001 <anandh.andrews@zucisystems.com>
2024-09-12 23:18:03 +0530

    paper classification changes for summary audit in the pipeline

--- src/main/java/in/handyman/raven/lib/model/utmodel/UrgencyTriageOutputTable.java
+++ src/main/java/in/handyman/raven/lib/model/utmodel/UrgencyTriageOutputTable.java
@@ -40,7 +40,7 @@ public class UrgencyTriageOutputTable implements CoproProcessor.Entity {
 
     @Override
     public List<Object> getRowData() {
-        return Stream.of(this.createdUserId, this.lastUpdatedUserId, this.processId, this.groupId, this.tenantId, this.confScore,
+        return Stream.of(this.createdOn, this.createdUserId,this.lastUpdatedOn, this.lastUpdatedUserId, this.processId, this.groupId, this.tenantId, this.confScore,
                 this.originId, this.paperNo, this.templateId, this.modelId, this.status, this.stage,
                 this.message, this.utResult, this.bbox, this.rootPipelineId, this.modelName, this.modelVersion,
                 this.batchId, this.createdOn, this.lastUpdatedOn).collect(Collectors.toList());
