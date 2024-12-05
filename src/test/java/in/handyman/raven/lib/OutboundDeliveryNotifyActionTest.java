52cb61878e77e957abffade85e100c0d7463168f
balasoundarya-thangapandi <balasoundarya.thanga@zucisystems.com>
2024-12-05 19:21:38 +0530

    outboundNotify

--- src/test/java/in/handyman/raven/lib/OutboundDeliveryNotifyActionTest.java
+++ src/test/java/in/handyman/raven/lib/OutboundDeliveryNotifyActionTest.java
@@ -43,12 +43,12 @@ class OutboundDeliveryNotifyActionTest {
                 .name("Outbound delivery notify")
                 .condition(true)
                 .documentId("166730399_C1")
-                .inticsZipUri("")
+                .inticsZipUri("http://192.168.14.15:5000/receive-payload")
                 .resourceConn("intics_zio_db_conn")
                 .zipChecksum("3t4ergfe32q345r6ty")
                 .querySet("\n" +
                         "select file_name as file_name, '' as file_uri,'' as zip_file_check_sum,\n" +
-                        " '' as endpoint,'${outbound.appId}' as app_id,'${outbound.appKeyId}' as app_secret_key,product_json as outbound_json,'Product' as outbound_condition\n" +
+                        " 'http://192.168.14.15:5000/receive-payload' as endpoint,'${outbound.appId}' as app_id,'${outbound.appKeyId}' as app_secret_key,product_json as outbound_json,'Product' as outbound_condition\n" +
                         "    from product_outbound.product_outbound_zip_file_input  \n" +
                         "   where group_id=43")
                 .build();
@@ -79,10 +79,10 @@ class OutboundDeliveryNotifyActionTest {
                 .resourceConn("intics_zio_db_conn")
                 .zipChecksum("3t4ergfe32q345r6ty")
                 .querySet("select ifd.file_name as file_name, '' as file_uri, '' as zip_file_check_sum,\n" +
-                        "'' as endpoint, \n" +
+                        "'http://0.0.0.0:5001/messages' as endpoint, \n" +
                         "pozfi.product_json as outbound_json, \n" +
                         "'ELV' as outbound_condition, pozfi.batch_id, ifd.document_id as document_id,\n" +
-                        "'AES' as encryption_type, ifd.\"uuid\" as \"uuid\" , 'RgF7I3z5FC8k9HkKUm3Htb1HhZPBczdksVr9fqGbTwc=' as encryption_key\n" +
+                        "'AES' as encryption_type, ifd.\"uuid\" as \"uuid\" , 'RgF7I3z5FC8k9HkKUm3Htb1HhZPBczdk' as encryption_key\n" +
                         "from product_outbound.product_outbound_zip_file_input pozfi join inbound_config.ingestion_file_details ifd \n" +
                         "on pozfi.file_name = ifd.file_name\n" +
                         "where pozfi.group_id=43 and pozfi.tenant_id = 115 and pozfi.batch_id = 'BATCH-43_0' and  ifd.transaction_id = 'TRZ-653'")
