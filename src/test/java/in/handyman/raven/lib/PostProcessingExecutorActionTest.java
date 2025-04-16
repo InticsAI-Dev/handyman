cb5999fab8126a458d6ed66a1436b75eaaf53fdb
anandhh2001 <anandh.andrews@zucisystems.com>
2025-04-16 05:50:18 +0530

    1. post processing encryption bug fix and optimized the code. 2. updated the input select query for post processing with reference to the sor meta.

--- src/test/java/in/handyman/raven/lib/PostProcessingExecutorActionTest.java
+++ src/test/java/in/handyman/raven/lib/PostProcessingExecutorActionTest.java
@@ -18,13 +18,38 @@ class PostProcessingExecutorActionTest {
                 .condition(true)
                 .outputTable("score.aggregation_evaluator")
                 .resourceConn("intics_zio_db_conn")
-                .querySet("SELECT 1 as tenantId, 0 as aggregated_score, 0 as masked_score , a.origin_id, a.paper_no, a.extracted_value, b.vqa_score,\n" +
-                        "'1' as rank, '1' as sor_item_attribution_id, a.sor_item_name, '1' as documentId, 1 as acc_transaction_id, b_box, '1113' as root_pipeline_id, \n" +
-                        "a.freq as frequency, b.question_id , b.synonym_id, a.model_registry\n" +
-                        "FROM voting.weighted_value a\n" +
-                        "JOIN score.scalar_validation b ON  a.validation_id = b.validation_id\n" +
-                        "join sor_transaction.sor_transaction_payload_queue_archive st on st.origin_id=a.origin_id and st.batch_id = a.batch_id\n" +
-                        "WHERE a.group_id  = '111' and a.weight_rank =1  and a.tenant_id = 1 and a.batch_id = 'BATCH-111_0';")
+                .querySet("SELECT\n" +
+                        "                     a.tenant_id as tenant_id,\n" +
+                        "                     a.confidence_score as aggregated_score,\n" +
+                        "                     a.weight_score as masked_score,\n" +
+                        "                     a.origin_id,\n" +
+                        "                     a.paper_no,\n" +
+                        "                     a.extracted_value,\n" +
+                        "                     b.vqa_score,\n" +
+                        "                     a.weight_rank as rank,\n" +
+                        "                     smca.sor_item_attribution_id as sor_item_attribution_id,\n" +
+                        "                     a.sor_item_name,\n" +
+                        "                     '${document_id}' as documentId,\n" +
+                        "                     1 as acc_transaction_id,\n" +
+                        "                     b.b_box,\n" +
+                        "                     a.root_pipeline_id as root_pipeline_id,\n" +
+                        "                     a.freq as frequency,\n" +
+                        "                     b.question_id,\n" +
+                        "                     b.synonym_id,\n" +
+                        "                     a.model_registry,\n" +
+                        "                     smca.is_encrypted,\n" +
+                        "                     smca.encryption_policy\n" +
+                        "                   FROM\n" +
+                        "                     voting.weighted_value a\n" +
+                        "                     JOIN score.scalar_validation b ON a.validation_id = b.validation_id\n" +
+                        "                     JOIN macro.sor_meta_consolidated_audit smca ON smca.tenant_id = b.tenant_id\n" +
+                        "                                     AND smca.root_pipeline_id = b.root_pipeline_id\n" +
+                        "                                     AND smca.synonym_id = b.synonym_id\n" +
+                        "                                     AND smca.sor_item_name = b.sor_item_name\n" +
+                        "                     JOIN sor_transaction.sor_transaction_payload_queue_archive st ON st.origin_id = a.origin_id\n" +
+                        "                                     AND st.batch_id = a.batch_id\n" +
+                        "                                     AND st.tenant_id = b.tenant_id\n " +
+                        " WHERE a.origin_id ='ORIGIN-396' and smca.line_item_type <> 'multi_value' and b.synonym_id in (23543,25134);")
                 .build();
 
         final ActionExecutionAudit action = ActionExecutionAudit.builder()
@@ -34,6 +59,7 @@ class PostProcessingExecutorActionTest {
         action.getContext().put("batch_id", "BATCH-113_0");
         action.getContext().put("created_user_id", "1");
         action.getContext().put("outbound.mapper.bsh.class.order", "NonLookUpFieldsMixerValidator");
+        action.getContext().put("pipeline.end.to.end.encryption", "true");
         action.getContext().put("NonLookUpFieldsMixerValidator", "import org.slf4j.Logger;\n" +
                 "import org.slf4j.LoggerFactory;\n" +
                 "\n" +
