d189a7a586810b43115d7179e0f3862504935c55
dhanekula.manohar@zucisystems.com <dhanekula.manohar@zucisystems.com>
2025-08-01 00:25:12 +0530

    DeepSift macro completed.

---
+++ src/main/java/in/handyman/raven/lib/model/deep/sift/DeepSiftLineItem.java
@@ -0,0 +1,36 @@
+package in.handyman.raven.lib.model.deep.sift;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class DeepSiftLineItem {
+    private String model;
+    @JsonProperty("infer_response")
+    private String inferResponse;
+    @JsonProperty("confidence_score")
+    private Integer confidenceScore;
+    private String bboxes;
+    private String originId;
+    private Integer paperNo;
+    private Long processId;
+    private Long rootPipelineId;
+    private Long groupId;
+    private Long actionId;
+    private Long tenantId;
+    private String inputFilePath;
+    private String batchId;
+    private String base64Img;
+    @JsonProperty("imageDPI")
+    private Long imageDPI;
+    private Long imageWidth;
+    private Long imageHeight;
+    private String extractedImageUnit;
+    private String processName;
+}
