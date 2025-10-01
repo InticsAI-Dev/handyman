7830bb44fde48a634ab1aa140b663a7d78a642c8
anandh.andrews <anandh.andrews@zucisystems.com>
2025-10-02 02:15:02 +0530

    enhancement: refactor Azure database connection handling to use HikariJdbiProvider

--- src/main/java/in/handyman/raven/core/azure/adapters/HikariJdbiProvider.java
+++ src/main/java/in/handyman/raven/core/azure/adapters/HikariJdbiProvider.java
@@ -16,8 +16,8 @@ import java.util.Objects;
 public class HikariJdbiProvider {
 
     private static final Logger log = LoggerFactory.getLogger(HikariJdbiProvider.class);
-    public static final int MIN_IDLE = Integer.parseInt(PropertyHandler.getOrDefault("azure.identity.hcp.minimum.idle","20"));
-    public static final int MAX_POOL_SIZE = Integer.parseInt(PropertyHandler.getOrDefault("azure.identity.hcp.max.pool.size","20"));;
+    public static final int MIN_IDLE = Integer.parseInt(PropertyHandler.getOrDefault("azure.identity.hcp.minimum.idle","100"));
+    public static final int MAX_POOL_SIZE = Integer.parseInt(PropertyHandler.getOrDefault("azure.identity.hcp.max.pool.size","300"));;
     public static final int CONNECTION_TIMEOUT_MS = Integer.parseInt(PropertyHandler.getOrDefault("azure.identity.hcp.conn.timeout","30000"));
     public static final int IDLE_TIMEOUT_MS = Integer.parseInt(PropertyHandler.getOrDefault("azure.identity.hcp.idle.timeout","35000"));;
     public static final int MAX_LIFETIME_MS = Integer.parseInt(PropertyHandler.getOrDefault("azure.identity.hcp.max.lifetime","45000"));;
@@ -73,7 +73,7 @@ public class HikariJdbiProvider {
         if (hikariDataSource != null) {
             HikariPoolMXBean poolMXBean = hikariDataSource.getHikariPoolMXBean();
             if (poolMXBean != null) {
-                log.info("HikariCP Metrics - Active Connections: {}, Idle Connections: {}, Total Connections: {}, Threads Awaiting Connection: {}",
+                log.debug("HikariCP Metrics - Active Connections: {}, Idle Connections: {}, Total Connections: {}, Threads Awaiting Connection: {}",
                         poolMXBean.getActiveConnections(),
                         poolMXBean.getIdleConnections(),
                         poolMXBean.getTotalConnections(),
