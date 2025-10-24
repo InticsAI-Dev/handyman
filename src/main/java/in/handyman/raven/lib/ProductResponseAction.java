266d1b19a920a9c6bfc302a3e85947cd4bbe9d3b
Manikandan-Thangaraj-ZS0321 <manikandan.tm@zucisystems.com>
2025-10-24 11:19:35 +0530

    enhancement: make consumer API count configurable in ProductResponseAction

--- src/main/java/in/handyman/raven/lib/ProductResponseAction.java
+++ src/main/java/in/handyman/raven/lib/ProductResponseAction.java
@@ -90,7 +90,7 @@ public class ProductResponseAction implements IActionExecution {
                             new ProductResponseAction.ProductResponseInputTable(), urls, action);
             coproProcessor.startProducer(productResponse.getQuerySet(), Integer.valueOf(action.getContext().get(DB_SELECT_READ_BATCH_SIZE)));
             Thread.sleep(1000);
-            int consumerApiCountStr = Integer.parseInt(action.getContext().getOrDefault("alchemy.response.consumer.API.count", "10"));
+            int consumerApiCountStr = Integer.parseInt(action.getContext().getOrDefault("alchemy.response.consumer.API.count", "12"));
             coproProcessor.startConsumer(insertQuery, consumerApiCountStr, Integer.valueOf(action.getContext().get(DB_INSERT_WRITE_BATCH_SIZE)), new ProductResponseAction.ProductResponseConsumerProcess(log, aMarker, action));
             log.info(aMarker, "Product Response has been completed {}  ", productResponse.getName());
         } catch (Exception t) {
