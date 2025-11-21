da61e422a9e5566f2f63370becd19958e337d1cf
anandh.andrews <anandh.andrews@zucisystems.com>
2025-11-21 10:51:46 +0530

    enhancement: handle empty priority labels in label processing logic

--- src/main/java/in/handyman/raven/lib/adapters/selections/LabelWithPriorityProcessor.java
+++ src/main/java/in/handyman/raven/lib/adapters/selections/LabelWithPriorityProcessor.java
@@ -247,7 +247,19 @@ public class LabelWithPriorityProcessor {
         if (json == null || json.isBlank()) return Map.of();
 
         try {
-            return mapper.readValue(json, new TypeReference<Map<String, Integer>>() {});
+            Map<String, Integer> output = mapper.readValue(json, new TypeReference<Map<String, Integer>>() {});
+
+            Map<String, Integer> temp = new HashMap<>();
+
+            output.forEach((key, value) -> {
+                temp.put(removeSpecialCharacters(key), value);
+            });
+
+            output.clear();
+            output.putAll(temp);
+
+            return output;
+
         } catch (Exception e) {
             return Map.of();
         }
@@ -255,16 +267,24 @@ public class LabelWithPriorityProcessor {
 
     private void assignPriorities(List<SelectionFilteringInputTable> rows, Map<String, Integer> priorityMap) {
         rows.forEach(r -> {
-            String key = r.getSorItemLabel();
+            String key = removeSpecialCharacters(r.getSorItemLabel());
             int p = priorityMap.getOrDefault(key, Integer.MAX_VALUE);
             r.setLabelPriorityIdx(String.valueOf(p));
         });
     }
 
+
+    public String removeSpecialCharacters(String input) {
+        if (input == null) return "";
+        return input.replaceAll("[^a-zA-Z0-9]", "").toLowerCase().trim();
+    }
+
     // ========================= STAGE 5: MESSAGES =========================
     private String appendMsg(SelectionFilteringInputTable row, String message) {
+        String msgWithPriority = "[p=" + row.getLabelPriorityIdx() + "] " + message;
         String existing = row.getLabelMatchMessage();
-        if (existing == null || existing.isBlank()) return message;
-        return existing + " | " + message;
+        if (existing == null || existing.isBlank()) return msgWithPriority;
+        return existing + " | " + msgWithPriority;
     }
+
 }
