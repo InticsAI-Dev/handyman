2058a582008bd4002e9a04c7678fda692c2f9fa1
christopher.paulraj@zucisystems.com <christopher.paulraj@zucisystems.com>
2026-02-04 00:22:13 +0530

    input pojo added in bsh/model/PostProcessingExecutorInput

---
+++ src/main/java/in/handyman/raven/lib/model/bsh/PostProcessingExecutorInput.java
@@ -0,0 +1,37 @@
+package in.handyman.raven.lib.model.bsh;
+
+public class PostProcessingExecutorInput {
+
+    private Long tenantId;
+    private double aggregatedScore;
+    private double maskedScore;
+    private String originId;
+    private Integer paperNo;
+    private String extractedValue;
+    private double vqaScore;
+    private Integer rank;
+    private Integer sorItemAttributionId;
+    private String sorItemName;
+    private String documentId;
+    private Long accTransactionId;
+    private String label;
+    private String sectionAlias;
+    private Long score;
+    private String bBox;
+    private Long rootPipelineId;
+    private Long frequency;
+    private Long questionId;
+    private Long synonymId;
+    private String modelRegistry;
+    private String encryptionPolicy;
+    private String isEncrypted;
+    private String lineItemType;
+
+    public String getExtractedValue() {
+        return extractedValue;
+    }
+
+    public void setExtractedValue(String extractedValue) {
+        this.extractedValue = extractedValue;
+    }
+}
