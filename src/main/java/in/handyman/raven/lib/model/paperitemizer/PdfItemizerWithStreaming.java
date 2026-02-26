e5a3c0e94b25c547f5ff8dba62bfd02562120f17
hemantho.rabert@zucisystems.com <hemantho.rabert@zucisystems.com>
2026-02-27 03:05:27 +0530

    Added the limit for the Java PaperItemizer

--- src/main/java/in/handyman/raven/lib/model/paperitemizer/PdfItemizerWithStreaming.java
+++ src/main/java/in/handyman/raven/lib/model/paperitemizer/PdfItemizerWithStreaming.java
@@ -24,6 +24,7 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.NoSuchElementException;
+import java.util.Optional;
 
 public class PdfItemizerWithStreaming {
     public static final String PROCESS_NAME = PipelineName.PAPER_ITEMIZER.getProcessName();
@@ -35,6 +36,8 @@ public class PdfItemizerWithStreaming {
     static final String PAPER_ITEMIZER_OUTPUT_FORMAT = "paper.itemizer.output.format";
     static final String PAPER_ITEMIZER_FILE_DPI = "paper.itemizer.file.dpi";
     static final String PAPER_ITEMIZATION_RESIZE_ACTIVATOR = "paper.itemization.resize.activator";
+    private static final String PAPER_ITEMIZER_FIXED_PAGE_ENABLER = "paper.itemizer.fixed.page.enabler";
+    private static final String PAPER_ITEMIZER_FIXED_PAGE_COUNT = "paper.itemizer.fixed.page.count";
     static final String MODEL_NAME = "APP";
     static final String VERSION = "1";
     private final ActionExecutionAudit action;
@@ -135,6 +138,8 @@ public class PdfItemizerWithStreaming {
         final int imageWidthSetting = Integer.parseInt(imageWidth);
         final String imageHeight = getContextVariableWithDefault(PAPER_ITEMIZER_RESIZE_HEIGHT, "2550");
         final int imageHeightSetting = Integer.parseInt(imageHeight);
+        final boolean fixedPageEnabler = Boolean.parseBoolean(getContextVariableWithDefault(PAPER_ITEMIZER_FIXED_PAGE_ENABLER, "true"));
+        final int fixedPageSize = Integer.parseInt(getContextVariableWithDefault(PAPER_ITEMIZER_FIXED_PAGE_COUNT, "25"));
 
         Path path = Paths.get(pdfPath);
 
@@ -144,9 +149,18 @@ public class PdfItemizerWithStreaming {
 
             final String originalName = getFileNameFromPath(pdfPath);
             final String normalizedFormat = getContextVariableWithDefault(PAPER_ITEMIZER_OUTPUT_FORMAT, "jpg").toLowerCase();
-            final int pageCount = getPageNo(document.getNumberOfPages());
 
-            for (int i = 0; i < document.getNumberOfPages(); i++) {
+            int pageCount = 0;
+            if (fixedPageEnabler){
+                pageCount = fixedPageSize;
+                log.info("Fixed page itemization enabled. Each output image will contain up to {} pages. Total output pages for file {}: {}", fixedPageSize, originalName, pageCount);
+            }
+            else {
+                pageCount = document.getNumberOfPages();
+                log.info("Fixed page itemization disabled. Each output image will contain 1 page. Total output pages for file {}: {}", originalName, pageCount);
+            }
+
+            for (int i = 0; i < pageCount; i++) {
                 ImageType imageType = ImageType.RGB.toString().equalsIgnoreCase(IMAGE_TYPE) ? ImageType.RGB : ImageType.GRAY;
                 BufferedImage image = renderer.renderImageWithDPI(i, imageDpiSetting, imageType);
                 if (imageResizeEnableSetting) {
