46609034274e855fccb566b0c632412fc6d896ff
Balasoundarya <balasoundarya.thanga@zucisystems.com>
2026-04-06 17:07:31 +0530

    Fix: DB connection handling to use resource name specific credentials instead of shared pool

---
+++ src/main/java/in/handyman/raven/core/azure/adapters/ResourceConfigJdbiProvider.java
@@ -0,0 +1,29 @@
+package in.handyman.raven.core.azure.adapters;
+
+import in.handyman.raven.exception.HandymanException;
+import in.handyman.raven.lambda.access.repo.HandymanRepoImpl;
+import in.handyman.raven.lambda.doa.config.SpwResourceConfig;
+import org.jdbi.v3.core.Jdbi;
+
+public class ResourceConfigJdbiProvider {
+
+    private final SpwResourceConfig resourceConfig;
+
+    public ResourceConfigJdbiProvider(SpwResourceConfig resourceConfig) {
+        this.resourceConfig = resourceConfig;
+    }
+
+    public Jdbi get() {
+        return Jdbi.create(resourceConfig.getResourceUrl(), resourceConfig.getUserName(), resourceConfig.getPassword());
+    }
+
+    public static SpwResourceConfig fetchResourceConfig() {
+        HandymanRepoImpl repo = new HandymanRepoImpl();
+        return repo.findAllResourceConfigs().stream()
+                .findFirst()
+                .map(c -> repo.getResourceConfig(c.getConfigName()))
+                .orElseThrow(() -> new HandymanException(
+                        "No resource config found in spw_resource_config"));
+    }
+
+}
